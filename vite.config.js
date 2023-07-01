65a9a2a85b9f9b711aa788c200bd5844ee1c73cb
PrapatsornT <Tanggoma@Tans-MBP.local>
2023-06-30 21:53:01 -0400

    fix base vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/block24-react/',
+  base: '/Puppy_list_react/',
   plugins: [react()],
 })
